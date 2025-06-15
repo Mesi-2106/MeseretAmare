a0a1b689487122cbf54737f87586a6488cbb87de
Mesi-2106 <amaremeseret381@gmail.com>
2025-06-15 12:59:55 +0300

    timetable-m

---
+++ timetable.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<=12;i++)
+    {
+        for(int j=0;j<=12;j++){
+		
+    cout<<" "<<i *j;
+    }
+	cout<<endl;
+	}
+    return 0;
+}
